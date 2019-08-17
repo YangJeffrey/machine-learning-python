6be4a1c534cc263045fb5516b884dc2da76d6f77
Jeffrey Yang <36713824+YangJeffrey@users.noreply.github.com>
2019-08-17 15:44:06 -0400

    Update tfRNN.py

--- tfRNN.py
+++ tfRNN.py
@@ -7,6 +7,7 @@ import os
 from sklearn import preprocessing
 from collections import deque
 import random
+import time
 
 SEQ_LEN = 60
 FUTURE_PERIOD_PREDICT = 3
@@ -39,6 +40,37 @@ def preprocess_df(df):
 
     random.shuffle(sequential_data)
 
+    buys = []
+    sells = []
+
+    for seq, target in sequential_data:
+        if target == 0:
+            sells.append([seq, target])
+        elif target == 1:
+            buys.append([seq, target])
+
+    random.shuffle(buys)
+    random.shuffle(sells)
+
+    lower = min(len(buys), len(sells))
+
+    buys = buys[:lower]
+    sells = sells[:lower]
+
+    sequential_data = buys + sells
+    random.shuffle(sequential_data)
+
+    X = []
+    y = []
+
+    for seq, target in sequential_data:
+        X.append(seq)
+        y.append(target)
+
+    return np.array(X), y
+
+
+
 main_df = pd.DataFrame()
 
 ratios = ["BTC-USD", "LTC-USD", "ETH-USD", "BCH-USD"]
@@ -57,9 +89,7 @@ for ratio in ratios:
         main_df = main_df.join(df)
 
 main_df['future'] = main_df[f"{RATIO_TO_PREDICT}_close"].shift(-FUTURE_PERIOD_PREDICT)
-
 main_df['target'] = list(map(classify, main_df[f"{RATIO_TO_PREDICT}_close"], main_df["future"]))
-
 #print(main_df[[f"{RATIO_TO_PREDICT}_close", "future", "target"]].head(10))
 
 times = sorted(main_df.index.values)
@@ -68,5 +98,9 @@ last_5pct = times[-int(0.05 * len(times))]
 validation_main_df = main_df[(main_df.index >= last_5pct)]
 main_df = main_df[(main_df.index < last_5pct)]
 
-#train_x, train_y = preprocess_df(main_df)
-#validation_x, validation_y = preprocess_df(validation_main_df)
+train_x, train_y = preprocess_df(main_df)
+validation_x, validation_y = preprocess_df(validation_main_df)
+
+print(f"train data: {len(train_x)} validation: {len(validation_x)}")
+print(f"dont buys: {train_y.count(0)}, buys: {train_y.count(1)}")
+print(f"VAL dont buys: {validation_y.count(0)}, VAL buys: {validation_y.count(1)}")
